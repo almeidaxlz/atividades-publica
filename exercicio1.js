247998270e78d63ea151409896dd4ebf01998336
almeidaxlz <162449595+almeidaxlz@users.noreply.github.com>
2025-02-25 12:16:07 -0300

    Add files via upload

---
+++ exercicio1.js
@@ -0,0 +1,14 @@
+let valor1 = 55
+let valor2 = 35
+
+let mult = valor1 * valor2
+
+console.log("Multiplicação = " + mult)
+
+ exercicio 2
+ 
+let av1 = 10.0
+let av2 = 7.0
+let av3 = 6.0
+
+let div = av1 + av2 + av3
