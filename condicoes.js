ef75d0abaee0b142b633c9cd47388e59028e9943
almeidaxlz <162449595+almeidaxlz@users.noreply.github.com>
2025-03-10 08:47:07 -0300

    Update condicoes.js

--- condicoes.js
+++ condicoes.js
@@ -7,11 +7,12 @@ console.log("a quantidade ideal de agua é" + agua_l + "litros")
 
 // exercício 2:
 let av1 = 10.0
-let av2 = 70.0
-let av3 = 40.0
+let av2 = 7
+let av3 = 4
 const media = ( av1 + av2 + av3 )/3
 if(media>=6.0){
     console.log("aprovado");
 }else{
     console.log("reprovado");
-}
+    }
+console.log("a media é " + media)
