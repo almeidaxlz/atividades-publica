5c3bae391d154e9c460861d809ad39292ead8a3c
almeidaxlz <162449595+almeidaxlz@users.noreply.github.com>
2025-04-28 07:38:20 -0300

    Update times.js

--- times.js
+++ times.js
@@ -1,37 +1,47 @@
 let times = require("./dados.json");
-function vertimes (){
-      times.forEach ((time)=>{
-        console.log (time.nome + "-" + time.goleiro + "(goleiro)," + time.alaDireita + "(alaDireita)," + time.alaEsquerda + "(alaEsquerda)," + time.pivo + "(pivo)," + time.fixo + "(fixo)," + time.tecnico + "(tecnico)," + time.vitorias + "(vitorias)," + time.derrotas + "(derrotas),")
-      });
 
-}
-vertimes();
+vertime();
+    function vertime (){
+        times.forEach ((time)=>{
+            console.log(time.nome + "-" + time.goleiro + "(goleiro)," + time.alaDireita + "(alaDireita)," + time.alaEsquerda + "(alaEsquerda)," + time.pivo + "(pivo)," + time.fixo + "(fixo)," + time.tecnico + "(tecnico)," + time.vitorias + "(vitorias)," + time.derrotas + "(derrotas),")
+        });
+    }
+    vertime();
 
-function vertimes(){
+    function vertime(){
 
-}
+    }
+    function criarArquivo(){
+    let dadosEmTexto = JSON.stringify(times);       
+    const fs = require ("fs");
+    fs.writeFileSync("dados.json",dadosEmTexto);
+    console.log("Arquivo criado");
+    }
 
-
-function criarArquivo(){
-    let dadosEmTexto = JSON.stringify(times);
-    const fs = require("fs");
-    fs.writeFileSync("dados.json", dadosEmTexto);
-console.log("Arquivo criado");
-}
 
 function marcarJogos(timeA, timeB, data, horario, estadio, cidade){
-   let jogos = require("./jogos.json");
-   jogo = {
+let jogos = require("./jogos.json");
+jogo = {
     timeA: timeA,
     timeB: timeB,
     data: data,
     horario: horario,
     estadio: estadio,
-    cidade: cidade
-
-   }
+    cidade: cidade, 
+}
+jogos.push(jogo);
+jogosJSON = JSON.stringify(jogos);
+const fs = require('fs');
+fs.writeFileSync("jogos.json", jogosJson)
 
 }
-criarArquivo();
+marcarJogos("real madrugas", "leverkusen", "25/04/2025","alianz parque", "São Paulo" )
 
-vertimes();
+function mostrarJogos(){
+    let jogos = require("./jogos.json")
+    jogos.forEach ((jogo) => {
+        console.log(jogo.timeA + "x" + jogo.timeB + "jogo.data" + "jogo.estadio" + "jogo.cidade")
+    }
+    )
+} 
+mostrarJogos();
